64c14728f77a853a5d1e00947a72848cb87ee327
rsuregar <rsuregar@gmail.com>
2022-02-19 11:33:43 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
+import { useApi } from './config/useApi';
+import Layout from './layouts/Layout';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Layout>
+      <h1>Wkwkwk</h1>
+    </Layout>
+    </>
   );
 }
 
